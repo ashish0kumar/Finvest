827120752a8f98495c69d44274155c08bf279f44
Ashish Kumar <ashishkumar901336@gmail.com>
2024-07-20 23:22:06 +0530

    lifting userInput state up

--- src/components/UserInput.jsx
+++ src/components/UserInput.jsx
@@ -1,61 +1,43 @@
-import { useState } from "react";
-
-export default function UserInput() {
-    const [userInput, setUserInput] = useState({
-        initialInvestment: 10000,
-        annualInvestment: 1200,
-        expectedReturn: 6,
-        duration: 10
-    });
-
-    function handleChange(inputIdentifier, newValue) {
-        setUserInput(prevUserInput => {
-            return {
-                ...prevUserInput,
-                [inputIdentifier]: newValue
-            };
-        });
-    }
-
+export default function UserInput({ userInput, onChange }) {
     return (
         <section id="user-input">
             <div className="input-group">
                 <p>
                     <label>Initial Investment</label>
-                    <input 
-                        type="number" 
+                    <input
+                        type="number"
                         required
                         value={userInput.initialInvestment}
-                        onChange={(event) => handleChange("initialInvestment", event.target.value)} 
+                        onChange={(event) => onChange("initialInvestment", event.target.value)}
                     />
                 </p>
                 <p>
                     <label>Annual Investment</label>
-                    <input 
-                        type="number" 
-                        required 
+                    <input
+                        type="number"
+                        required
                         value={userInput.annualInvestment}
-                        onChange={(event) => handleChange("annualInvestment", event.target.value)} 
+                        onChange={(event) => onChange("annualInvestment", event.target.value)}
                     />
                 </p>
             </div>
             <div className="input-group">
                 <p>
                     <label>Expected Return</label>
-                    <input 
-                        type="number" 
-                        required 
+                    <input
+                        type="number"
+                        required
                         value={userInput.expectedReturn}
-                        onChange={(event) => handleChange("expectedReturn", event.target.value)} 
+                        onChange={(event) => onChange("expectedReturn", event.target.value)}
                     />
                 </p>
                 <p>
                     <label>Duration</label>
-                    <input 
-                        type="number" 
-                        required 
+                    <input
+                        type="number"
+                        required
                         value={userInput.duration}
-                        onChange={(event) => handleChange("duration", event.target.value)} 
+                        onChange={(event) => onChange("duration", event.target.value)}
                     />
                 </p>
             </div>
